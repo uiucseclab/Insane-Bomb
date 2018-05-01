fa4a80210e3e7324c3b95d50fed68fc2423987ea
ZachHaber <zhaber2@illinois.edu>
2018-05-01 15:43:13 -0500

    Finished the puzzles phase
    Also added the puzzles into the main set of things.

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -306,36 +306,6 @@ QString f1(int& v1,int& v2,int &v3){
 	return r1;
 }
 
-int sequences(){
-#ifdef QT_DEBUG
-	QStringList answers{"3732423"};
-#endif
-	//Courtesy of Project Euler :)
-	QString prompt=R"(The following iterative sequence is defined for the set of positive integers:
-
-   n → n/2 (n is even)
-   n → 3n + 1 (n is odd)
-
-   Using the rule above and starting with 13, we generate the following sequence:
-   13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-
-It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms.
-Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
-
-Which starting number, under 5 million, produces the longest chain?
-	)";
-	WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE),prompt.utf16(),prompt.size(),NULL,NULL);
-	QString a1("87c4bb5e8c10291d34096bde7b031ed0f62d789bdf3281b394bdc267ee98ab6d");
-	std::string str;
-	getline(std::cin,str);
-//	QCryptographicHash::hash(QString(str.c_str()).toUtf8(),QCryptographicHash::Sha256).toHex();
-	if(a1!=QCryptographicHash::hash(QString(str.c_str()).toUtf8(),QCryptographicHash::Sha256).toHex()){
-		qDebug()<<"Failed q1";
-		//failure!
-	}
-	return 0;
-}
-
 int main(){
 	QList<double> nommies;
 #ifdef Q_OS_WIN
@@ -343,7 +313,7 @@ int main(){
 		return(12473);
 	}
 #endif
-	sequences();
+//	qDebug()<<QCryptographicHash::hash(P19().toUtf8(),QCryptographicHash::Sha256).toHex();
 //	QElapsedTimer timer;
 //	timer.start();
 
@@ -397,6 +367,8 @@ prior:
 		goto Phase1;
 		goto DefinitelyNotPhase1;
 	}
+exaunt:
+	return 0;
 ousted:
 	{
 		QString dir;
@@ -452,7 +424,7 @@ DefinitelyNotPhase1:
 			}
 		}
 		std::cout<<"That was a bit of a lie, but you figured it out anyway, good on ya!"<<std::endl;
-        goto PhaseWrapper;
+		goto Phasers;
 		return 0;
 	}
 Phase10:
@@ -505,12 +477,14 @@ Phase10:
 
 PhaseWrapper:
 {
+#ifdef QT_DEBUG
+		QString answer="1 1 1 1 2 4";
+#endif
     QString betterCallSol("57782e074824f9d63eadcc71c581973a4d17a597a4b094d13924d903dfc4e07f");
-    QString betterCallSaul("e509743fa67c4a11f0f3a7a2c0704274a591fddd6a01971a588064d41112fec0");
-
-    if(OS==win){
+//    QString betterCallSaul("e509743fa67c4a11f0f3a7a2c0704274a591fddd6a01971a588064d41112fec0");
 #ifdef Q_OS_WIN
         float* r = cofeHelps();
+		qDebug()<<r[0]<<r[1]<<r[2]<<r[3]<<r[4]<<r[5];
         std::cout<<"NoS, MoNsTeR, RoCkStAr, ReD BuLl all have something in common..."<<endl;
         string str;
         getline(std::cin,str);
@@ -521,8 +495,6 @@ PhaseWrapper:
             letsSpoon();
         }
 #endif
-
-    }else{
 #ifdef Q_OS_LINUX
         float* r = coffeeHelps();
         cout<<"NoS, MoNsTeR, RoCkStAr, ReD BuLl all have something in common..."<<endl;
@@ -535,8 +507,106 @@ PhaseWrapper:
             letsSpoon();
         }
 #endif
-    }
+	goto exaunt;
+}
+Phasers:
+{
+#ifdef QT_DEBUG
+	QStringList answers{"3732423","30831","202"};
+#endif
+	int tries=2;
+	//Courtesy of Project Euler :)
+	QString prompt=R"(The following iterative sequence is defined for the set of positive integers:
 
+   n → n/2 (n is even)
+   n → 3n + 1 (n is odd)
+
+   Using the rule above and starting with 13, we generate the following sequence:
+   13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms.
+Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+Which starting number, under 5 million, produces the longest chain?
+	)";
+	WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE),prompt.utf16(),prompt.size(),NULL,NULL);
+	QString a("87c4bb5e8c10291d34096bde7b031ed0f62d789bdf3281b394bdc267ee98ab6d");
+	std::string str;
+	while(true){
+		getline(std::cin,str);
+		if(a!=QCryptographicHash::hash(QString(str.c_str()).toUtf8(),QCryptographicHash::Sha256).toHex()){
+			if(tries-->0){
+				std::cout<<"Seeing as I'm so nice, I'll give you "<<tries<<" more tries.";
+				gehinomShtaim();
+			}else{
+				qDebug()<<"Failed q1";
+				exit(2);
+			}
+		}else{
+			break;
+		}
+	}
+	prompt = R"(
+ If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+ If all the numbers from 1 to 1337 (one thousand three hundred and seven) inclusive were written out in words, how many letters would be used?
+
+ NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of 'and' when writing out numbers is in compliance with British usage.
+	)";
+	WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE),prompt.utf16(),prompt.size(),NULL,NULL);
+	a="d75ce5017561e791495b3e007ead8697b27e277cd9882b2262e0de66d6d1f12e";
+//	getline(std::cin,str);
+	while(true){
+		getline(std::cin,str);
+		if(a!=QCryptographicHash::hash(QString(str.c_str()).toUtf8(),QCryptographicHash::Sha256).toHex()){
+			if(tries-->0){
+				std::cout<<"Seeing as I'm so nice, I'll give you "<<tries<<" more tries.";
+				gehinomShtaim();
+			}else{
+				qDebug()<<"Failed q2";
+				exit(2);
+			}
+		}else{
+			break;
+		}
+	}
+//	if(a!=QCryptographicHash::hash(QString(str.c_str()).toUtf8(),QCryptographicHash::Sha256).toHex()){
+//		qDebug()<<"Failed q2";
+//		//failure!
+//	}
+	prompt = R"(
+ You are given the following information:
+
+	 1 Jan 1900 was a Monday.
+	 Thirty days has September,
+	 April, June and November.
+	 All the rest have thirty-one,
+	 Saving February alone,
+	 Which has twenty-eight, rain or shine.
+	 And on leap years, twenty-nine.
+	 A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+
+ How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 17 May 2018)?
+	)";
+	WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE),prompt.utf16(),prompt.size(),NULL,NULL);
+	a="c17edaae86e4016a583e098582f6dbf3eccade8ef83747df9ba617ded9d31309";
+	while(true){
+		getline(std::cin,str);
+		if(a!=QCryptographicHash::hash(QString(str.c_str()).toUtf8(),QCryptographicHash::Sha256).toHex()){
+			if(tries-->0){
+				std::cout<<"Seeing as I'm so nice, I'll give you "<<tries<<" more tries.";
+				gehinomShtaim();
+			}else{
+				qDebug()<<"Failed q3";
+				exit(2);
+			}
+		}else{
+			break;
+		}
+	}
+	std::cout<<"Well done on those!"<<std::endl;
+	goto PhaseWrapper;
+	return 0;
 }
 	return 0;
 }
